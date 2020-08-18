aba0fced85b793bc7b18d4038a83e3911fb78898
Santanu Jana <40124834+StinkyAxe@users.noreply.github.com>
2020-08-19 03:47:49 +0530

    Add files via upload

---
+++ character_pattern.cpp
@@ -0,0 +1,35 @@
+/*
+Print the following pattern for the given N number of rows.
+Pattern for N = 4
+A
+BC
+CDE
+DEFG
+*/
+
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+
+ 
+    int n,i=1;
+    cin>>n;
+    while(i<=n)
+    {
+        int j=1;
+        char c='A'+(i)-1;
+        while(j<=i)
+        {
+            cout<<c;
+            j++;
+            c++;
+        }
+        cout<<endl;
+        i++;
+    }
+  
+}
+
