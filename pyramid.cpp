4988b9e31a29ae8c962a2458dc4523822b01e360
Santanu Jana <40124834+StinkyAxe@users.noreply.github.com>
2020-08-19 04:16:53 +0530

    Add files via upload

---
+++ pyramid.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i,j,k,n;
+	cin>>n;
+	while(i<=n) //Counting the number of rows.
+	{
+		j=n; // for the space we will consider j to be n to print space as a reverse triangle. 
+		while(j>i)
+		{
+			cout<<" ";
+			j--;
+		}
+		k=1; // Building the star pattern 
+		while(k<=2*i-1) // (2*1-1=1,2*2-1=3...)
+		{
+			cout<<"*";
+			k++;
+		}
+		cout<<endl;
+		i++;
+	}
+}
